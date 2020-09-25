9f2ba56840e52ac7f2d84f0e927685a97ada6126
r00tMakesGames <adhyanmall@gmail.com>
2020-09-25 20:34:41 +0530

    Day 2

---
+++ AR-Portal/Assets/scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void furnished()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void empty()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void xray()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
---
+++ AR-Portal/Assets/scripts/back.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class back : MonoBehaviour
+{
+    public void back_button()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
